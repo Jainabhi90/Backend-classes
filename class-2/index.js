7b2da13a281ea325d51792fc5cd972434b0e4026
Abhi Jain <jainabhi90980@gmail.com>
2026-08-13 10:16:22 +0530

    class 2 creating server from express

--- class-2/index.js
+++ class-2/index.js
@@ -9,25 +9,56 @@
 
 
 
-// creating webserver 
+// creating webserver by nodejs
 
-let http = require("http")
+// let http = require("http")
 
-let server =  http.createServer((req,res)=>{
-        console.log(req.url,"hehe");
-        // res.write("hii       ")
+// let server =  http.createServer((req,res)=>{
+//         console.log(req.url,"hehe");
+//         // res.write("hii       ")
 
-        if(req.url == '/'){
-            res.write("from home / ")
-        }
-        else if(req.url == '/about'){
-            res.write("from /about  ")
-        }
-        res.end("helloo")
+//         if(req.url == '/'){
+//             res.write("from home / ")
+//         }
+//         else if(req.url == '/about'){
+//             res.write("from /about  ")
+//         }
+//         else if(req.url == '/contact'){
+//             res.write("from /contact  ")
+//         }
+//         res.end("helloo")
         
         
-    })
-    server.listen(1234,()=>{
-        console.log("server runinggg..");
+//     })
+//     server.listen(1234,()=>{
+//         console.log("server runinggg..");
         
-    })
+//     })
+
+
+
+
+// creating server from exoress
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("hi i am from /home")
+})
+app.get('/about',(req,res)=>{
+    res.send("hi i am from /about")
+})
+app.use(()=>{
+    console.log("hehehhe");
+    
+})
+app.get('contact',(req,res)=>{
+    res.send("hi i am from /contact")
+})
+
+app.listen(1234,()=>{
+    console.log("server runing...");
+    
+})
