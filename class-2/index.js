5912d41ce85bc9082afc79c098158cd1a619f766
Abhi Jain <jainabhi90980@gmail.com>
2026-08-13 09:45:57 +0530

    class-2 server created

--- class-2/index.js
+++ class-2/index.js
@@ -1,8 +1,24 @@
-let os = require('os')
-console.log(os.totalmem()/1024/1024/1024);
-console.log(os.freemem()/1024/1024/1024);
-console.log(os.cpus());
-console.log(os.arch());
-console.log(os.uptime()/3600);
+// Checking your server details can't do anything just to read 
+
+// let os = require('os')
+// console.log(os.totalmem()/1024/1024/1024);
+// console.log(os.freemem()/1024/1024/1024);
+// console.log(os.cpus());
+// console.log(os.arch());
+// console.log(os.uptime()/3600);
 
 
+
+// creating webserver 
+
+let http = require("http")
+
+let server =  http.createServer((req,res)=>{
+        res.end("helloo")
+        
+        
+    })
+    server.listen(1234,()=>{
+        console.log("server runinggg..");
+        
+    })
