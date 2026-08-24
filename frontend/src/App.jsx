2d0e9a5d66cd5e55a3f7cfa4d2ae9206417e295f
Abhi Jain <jainabhi90980@gmail.com>
2026-08-24 14:55:03 +0530

    class-5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,39 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 
 const App = () => {
+  let [apiData,setApiData] = useState([]);
   useEffect(()=>{
-    fetch("http://localhost:4000/").then((res)=>{
-      return res.json()
-    }).then((data)=>{
-      console.log(data);
+    async function api(){
+    let res = await axios.get("http://localhost:4000/")
+    console.log(res.data);
+    setApiData(res.data)
+    }
+    api()
+    
+    // fetch("http://localhost:4000/").then((res)=>{
+    //   return res.json()
+    // }).then((data)=>{
+    //   console.log(data);
+    //   setApiData(data);
       
-    })
+    // })
   },[])
   return (
-    <div>App</div>
+    <div>
+      {
+        apiData.map((a)=>{
+          return(<>
+              <h1>{a.id}</h1>
+              <h1>{a.name}</h1>
+              <h1>{a.cllg}</h1>
+           
+          </>)
+        })
+      }
+    </div>
   )
 }
 
