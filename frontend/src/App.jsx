6ed457c13e8189b78f719d61e2fc4fe24951d835
Abhi Jain <jainabhi90980@gmail.com>
2026-08-27 12:43:54 +0530

    class 07

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,35 +4,40 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 const App = () => {
-  let [apiData,setApiData] = useState([]);
-  useEffect(()=>{
-    async function api(){
-    let res = await axios.get("http://localhost:4000/")
+  let [apiData,setApiData] = useState('');
+  let [users,setUsers] = useState([]);
+  let [name,setName] = useState("");
+  let [email,setEmail] = useState("");
+  let [pass,setPass] = useState("");
+  useEffect(() => {
+    axios.get("http://localhost:3000/").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
+  async function login(){
+    let res = await axios.post("http://localhost:3000/login",{email, pass})
     console.log(res.data);
     setApiData(res.data)
-    }
-    api()
-    
-    // fetch("http://localhost:4000/").then((res)=>{
-    //   return res.json()
-    // }).then((data)=>{
-    //   console.log(data);
-    //   setApiData(data);
-      
-    // })
-  },[])
+  }
+  async function signup(){
+    let res = await axios.post("http://localhost:3000/signUp",{name, email, pass})
+    console.log(res.data);
+    setApiData(res.data)
+  }
   return (
     <div>
-      {
-        apiData.map((a)=>{
-          return(<>
-              <h1>{a.id}</h1>
-              <h1>{a.name}</h1>
-              <h1>{a.cllg}</h1>
-           
-          </>)
-        })
-      }
+      <input placeholder='name' onChange={(e)=>{setName(e.target.value)}}></input>
+      <input placeholder='email' onChange={(e)=>{setEmail(e.target.value)}}></input>
+      <input placeholder='pass' onChange={(e)=>{setPass(e.target.value)}}></input>
+      <button onClick={signup}>Signup</button>
+      <button onClick={login}>Login</button>
+      {users.map((user) => (
+        <div key={user._id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   )
 }
