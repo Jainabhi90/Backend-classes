ecc49d63b9125e376f0195a67c4a674f055d0b58
Abhi Jain <jainabhi90980@gmail.com>
2026-09-03 10:32:23 +0530

    class 8 frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,89 @@
 import axios from 'axios';
 import React from 'react'
 import { useState } from 'react'
-import { useEffect } from 'react'
 
 const App = () => {
-  let [apiData,setApiData] = useState('');
-  let [users,setUsers] = useState([]);
-  let [name,setName] = useState("");
-  let [email,setEmail] = useState("");
-  let [pass,setPass] = useState("");
-  useEffect(() => {
-    axios.get("http://localhost:3000/").then((res) => {
-      setUsers(res.data);
-    });
-  }, []);
+  let [apiData, setApiData] = useState('');
+  let [users, setUsers] = useState([]);
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [pass, setPass] = useState("");
+  let [role, setRole] = useState("user");
+  let [token , setToken] = useState("")
 
-  async function login(){
-    let res = await axios.post("http://localhost:3000/login",{email, pass})
+  async function login() {
+    let res = await axios.post("http://localhost:3000/login", {
+      email,
+      pass
+    })
+
+    console.log(res.data);
+    setApiData(res.data);
+
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+    }
+  }
+
+  async function signup() {
+    let res = await axios.post("http://localhost:3000/signUp", {
+      name,
+      email,
+      pass,
+      role
+    })
+
     console.log(res.data);
     setApiData(res.data)
   }
-  async function signup(){
-    let res = await axios.post("http://localhost:3000/signUp",{name, email, pass})
+
+  async function getApiData() {
+    let token = localStorage.getItem("token");
+
+    let res = await axios.get("http://localhost:3000/api", {
+      headers: {
+        Authorization: token
+      }
+    })
+
     console.log(res.data);
-    setApiData(res.data)
+    setApiData(res.data);
   }
+
   return (
     <div>
-      <input placeholder='name' onChange={(e)=>{setName(e.target.value)}}></input>
-      <input placeholder='email' onChange={(e)=>{setEmail(e.target.value)}}></input>
-      <input placeholder='pass' onChange={(e)=>{setPass(e.target.value)}}></input>
+      <input
+        placeholder="name"
+        onChange={(e) => { setName(e.target.value) }}
+      />
+
+      <input
+        placeholder="email"
+        onChange={(e) => { setEmail(e.target.value) }}
+      />
+
+      <input
+        placeholder="pass"
+        type="password"
+        onChange={(e) => { setPass(e.target.value) }}
+      />
+
+      <input
+        placeholder="role"
+        value={role}
+        onChange={(e) => { setRole(e.target.value) }}
+      />
+
       <button onClick={signup}>Signup</button>
       <button onClick={login}>Login</button>
-      {users.map((user) => (
-        <div key={user._id}>
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
-      ))}
+
+      {apiData && typeof apiData === "object" && (
+        <ul>
+          <li>Name: {apiData.name}</li>
+          <li>Email: {apiData.email}</li>
+        </ul>
+      )}
+
     </div>
   )
 }
